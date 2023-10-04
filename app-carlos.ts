917afde5cc7a292543a7506315331d47f4d7aae9
CABlank <carlosblank333@gmail.com>
2023-10-04 14:22:08 -0400

    Update app-carlos.ts

--- app-carlos.ts
+++ app-carlos.ts
@@ -12,6 +12,7 @@ interface CashFlow {
 interface CfArray {
   rowID: number;
   date: Date;
+  formattedDate: string;
   amount: number;
   kind: number;
   escrow: string;
@@ -36,21 +37,13 @@ interface Answer {
   DCFpv?: number;
 }
 
-function addMonths(date: Date, months: number): Date {
-  date.setMonth(date.getMonth() + months);
-  return date;
-}
-
-function parseDateFromString(dateStr: string): Date {
-  return new Date(dateStr);
-}
 
 
 
 
 function NewCashflowArray(pmts: CashFlow[], compounding: string): [AnnuityArray, Date | undefined] {
   const FreqMap: { [key: string]: number } = {
-      "Monthly": 12,
+      "Monthly": 1,
       // ... Other mappings
   };
 
@@ -67,7 +60,7 @@ function NewCashflowArray(pmts: CashFlow[], compounding: string): [AnnuityArray,
     console.log(`First payment date for row ${row}: ${firstPaymentDate}`);
     let amount = v.amount;
 
-    if (v.cfType === "Invest") {
+    if (v.cfType === "Return") {
         cfType = -1;
         if (!asOf) {
             asOf = firstPaymentDate;
@@ -78,33 +71,39 @@ function NewCashflowArray(pmts: CashFlow[], compounding: string): [AnnuityArray,
     
     console.log(`Processing cash flow of type: ${v.cfType} (kind: ${cfType})`);
 
-      pFreq = FreqMap[v.frequency] || 0;
-      if (pFreq === 0) {
-          pFreq = FreqMap[compounding];
-      }
-      pFreq = 12 / pFreq;
-      const escrow = v.escrow;
+    pFreq = FreqMap[v.frequency];
+    if (!pFreq) {
+        pFreq = FreqMap[compounding];
+    }
 
-      for (let j = 0; j < v.number; j++) {
-          aa.push({
-              rowID: row,
-              date: addMonths(firstPaymentDate, j * pFreq),
-              amount: amount,
-              kind: cfType,
-              escrow: escrow,
-              caseCode: v.caseCode,
-              owner: v.buyer,
-              pmtNmbr: row * v.number + j,
-              freq: v.frequency
-          });
-          console.log(`Pushed new cash flow array element for row ${row}, payment number: ${j}`);
-      }
+    const escrow = v.escrow;
+
+    for (let j = 0; j < v.number; j++) {
+      const currentDate = new Date(firstPaymentDate);
+      currentDate.setMonth(currentDate.getMonth() + j * pFreq);
+  
+      aa.push({
+        rowID: row,
+        date: currentDate,
+        formattedDate: formatDateToYYYYMMDD(currentDate),
+        amount: amount,
+        kind: cfType,
+        escrow: escrow,
+        caseCode: v.caseCode,
+        owner: v.buyer,
+        pmtNmbr: row * v.number + j,
+        freq: v.frequency
+      });
+      console.log(`Pushed new cash flow array element for row ${row}, payment number: ${j}`);
+  }
+  
   }
   aa.sort((a, b) => a.date.getTime() - b.date.getTime() || a.rowID - b.rowID);
 
   console.log("Final cash flow array:", aa);
   return [aa, asOf];
 }
+
 
 
 
@@ -126,6 +125,80 @@ function CalcAnnuity(annuity: Annuity): Answer {
   return result;
 }
 
+function paymentCount(cfs: CashFlow[]): number {
+  let sum = 0;
+  for (const v of cfs) {
+      sum += v.number;
+  }
+  return sum;
+}
+
+function compareDates(now: Date, prior: Date): boolean {
+  return prior > now;
+}
+
+function addMonths(date: Date, offset: number): Date {
+  let newDate = new Date(date);
+  newDate.setMonth(date.getMonth() + offset);
+  const dayOfMonth = date.getDate();
+  if (dayOfMonth > 28) {
+      newDate.setDate(28);
+  }
+  if (dayOfMonth === getDaysInMonth(date) || dayOfMonth > getDaysInMonth(newDate)) {
+      newDate.setDate(getDaysInMonth(newDate));
+  } else {
+      newDate.setDate(dayOfMonth);
+  }
+  return newDate;
+}
+
+function getDaysInMonth(date: Date): number {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+}
+
+function toFixed(num: number, precision: number): number {
+  const output = Math.pow(10, precision);
+  return Math.round(num * output) / output;
+}
+
+function before(value: string, a: string): string {
+  const pos = value.indexOf(a);
+  if (pos === -1) {
+      return "";
+  }
+  return value.substring(0, pos);
+}
+function parseDateFromString(dateString: string): Date {
+  const [year, month, day] = dateString.split('-').map(Number);
+  return new Date(year, month - 1, day);
+}
+
+function parseInventoryDateFormat(timeString: string): Date {
+  // You will need to implement the parsing logic for this format.
+  return new Date(timeString); // Placeholder
+}
+
+function parseFBDateFormat(timeString: string): Date {
+  // You will need to implement the parsing logic for this format.
+  return new Date(timeString); // Placeholder
+}
+
+function formatDateToYYYYMMDD(date: Date): string {
+  let dd: string | number = date.getDate();
+  let mm: string | number = date.getMonth() + 1; // January is 0!
+  const yyyy: number = date.getFullYear();
+  
+  if(dd < 10) {
+      dd = '0' + dd;
+  } 
+  
+  if(mm < 10) {
+      mm = '0' + mm;
+  } 
+
+  return `${yyyy}-${mm}-${dd}`;
+}
+
 const annuityData: Annuity = {
   cashFlows: [
       {
@@ -133,7 +206,7 @@ const annuityData: Annuity = {
           amount: 100,
           frequency: "Monthly",
           cfType: "Return",
-          number: 12,
+          number: 6,
           escrow: "",
           caseCode: "",
           buyer: ""
